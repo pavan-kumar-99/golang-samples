1fb4bb0e58baf2b8fecae3b189895bcb50b978a1
pavan-kumar-99 <pavan1999.kumar@gmail.com>
2021-07-11 19:46:17 +0530

    Adding secretctl cobra scripts

---
+++ cobra/secretctl/cmd/get_secret.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func Getsec(namespace string, secretname string) string {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	var k kubernetes.Interface
+	k, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	secret, err := k.CoreV1().Secrets(namespace).Get(context.TODO(), secretname, metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+	data := secret.Data
+	for k, v := range data {
+		val, err := json.Marshal(map[string]string{k: string(v)})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(val))
+	}
+	return ""
+}
---
+++ cobra/secretctl/go.mod
@@ -0,0 +1,10 @@
+module github.com/pavan-kumar-99/golang-samples/cobra/secretctl
+
+go 1.16
+
+require (
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/viper v1.8.1
+	k8s.io/apimachinery v0.21.2
+	k8s.io/client-go v0.21.2
+)
