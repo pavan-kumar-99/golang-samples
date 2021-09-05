44c9d435748b978b6172f1ceeb7abf796b961a8e
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2021-09-05 20:35:22 +0530

    Update aws_ebs.go

--- aws-go/s3/aws_ebs.go
+++ aws-go/s3/aws_ebs.go
@@ -1,4 +1,4 @@
-package main
+package aws
 
 import (
 	"fmt"
@@ -18,12 +18,39 @@ func init() {
 
 func main() {
 	volid := "vol-00e39e3eb62630e69"
+	fmt.Println(filter_vol_by_id(volid))
+	fmt.Println(filter_vol_by_tags("Name", "test"))
+}
+
+func filter_vol_by_id(volid string) (string, error) {
+
 	input := &ec2.DescribeVolumesInput{
 		VolumeIds: []*string{&volid},
 	}
 	vol, err := sess.DescribeVolumes(input)
 	if err != nil {
-		fmt.Println(err.Error())
+		return "", err
 	}
-	fmt.Println(*vol.Volumes[0].AvailabilityZone)
+	vol_az := *vol.Volumes[0].AvailabilityZone
+	return vol_az, nil
 }
+
+func filter_vol_by_tags(tag_key, tag_value string) (string, error) {
+	input := &ec2.DescribeVolumesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("tag:Name"),
+				Values: []*string{aws.String(tag_value)},
+			},
+		},
+	}
+	vol, err := sess.DescribeVolumes(input)
+	if err != nil {
+		return "", err
+	}
+	vol_az := *&vol.Volumes[0].VolumeId
+	return *vol_az, nil
+}
+
+//To-Do add Caching mechanism to cache the data from AWS
+//To add filtering of EBS volumes from multiple tags
